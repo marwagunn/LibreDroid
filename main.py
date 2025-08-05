d65270d25cf42e0451d29d32937526bf288e4974
marwagunn <riosmarwa@gmail.com>
2025-08-05 23:21:01 +0100

    send

---
+++ main.py
@@ -0,0 +1,28 @@
+import sys
+from stats import get_chars,get_words_count,get_words,sort_chars_dict
+
+def main():
+
+    if len(sys.argv) < 2:
+            print("Usage: python3 main.py <path_to_book>")
+            sys.exit(1)
+    book_path = sys.argv[1]
+
+    text = get_words(book_path)
+    num_words= get_words_count(text)
+    chars= get_chars( text)
+    chars_list= sort_chars_dict(chars)
+    print_report( book_path,num_words,chars_list)
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item['char'].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+
+main()
---
+++ stats.py
@@ -0,0 +1,32 @@
+def get_words(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+
+def get_words_count(text):
+    words = text.split()
+    return len(words)
+
+
+def get_chars(text):
+    chars={}
+
+    for c in text:
+        lowerC = c.lower()
+        if lowerC in chars:
+            chars[lowerC]+=1
+        else:
+            chars[lowerC]=1
+
+    return chars
+
+def sort_on(c):
+    return c["num"]
+
+
+def sort_chars_dict(chars):
+    chars_list= []
+    for c in chars :
+        chars_list.append({"char":c,"num":chars[c]})
+    chars_list.sort(reverse=True, key=sort_on)
+    return chars_list
